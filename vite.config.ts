3e50ae1ee8d69dff39b45de498b49611c79b2400
Serein <1431037397@qq.com>
2025-07-06 19:35:27 +0800

    GitHub Pages 添加重写规则

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "node:dns";
-import * as fs from "node:fs";
 
 // https://vite.dev/config/
 // vite.config.ts
 export default defineConfig({
   base: './', // 修改为相对路径
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-    // ✅ 构建完成后复制 index.html 为 404.html
-    emptyOutDir: true,
-  },
-  buildEnd() {
-    const indexPath = resolve(__dirname, 'dist/index.html');
-    const notFoundPath = resolve(__dirname, 'dist/404.html');
-    fs.copyFileSync(indexPath, notFoundPath);
-  },
 })
