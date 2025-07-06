2754fe607028390135efacaf4edd997392cf419d
Serein <1431037397@qq.com>
2025-07-06 15:40:05 +0800

    Fix:解决访问页面404问题

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.ts
 export default defineConfig({
-  base: '/my-blog/',
-  plugins: [react()],
+  base: './', // 修改为相对路径
+  plugins: [react()]
 })
