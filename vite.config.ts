c17c260cb7ee9068d2344d7e707dcc5ee4221900
Serein <1431037397@qq.com>
2025-07-07 16:58:51 +0800

    首页开发完成

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 import fs from 'fs'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-blog' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-blog/' : '/',
   plugins: [
     react(),
     {
